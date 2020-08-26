96adaa84724d1d11756b8c00f990e3bdd13bd160
RonanSilasSmith <smith.ronan@outlook.com>
2020-08-26 02:44:19 -0800

    adds functionality to search feature

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,50 +1,36 @@
-var response = {
-    "login": "octocat",
-    "id": 583231,
-    "node_id": "MDQ6VXNlcjU4MzIzMQ==",
-    "avatar_url": "https://avatars3.githubusercontent.com/u/583231?v=4",
-    "gravatar_id": "",
-    "url": "https://api.github.com/users/octocat",
-    "html_url": "https://github.com/octocat",
-    "followers_url": "https://api.github.com/users/octocat/followers",
-    "following_url": "https://api.github.com/users/octocat/following{/other_user}",
-    "gists_url": "https://api.github.com/users/octocat/gists{/gist_id}",
-    "starred_url": "https://api.github.com/users/octocat/starred{/owner}{/repo}",
-    "subscriptions_url": "https://api.github.com/users/octocat/subscriptions",
-    "organizations_url": "https://api.github.com/users/octocat/orgs",
-    "repos_url": "https://api.github.com/users/octocat/repos",
-    "events_url": "https://api.github.com/users/octocat/events{/privacy}",
-    "received_events_url": "https://api.github.com/users/octocat/received_events",
-    "type": "User",
-    "site_admin": false,
-    "name": "The Octocat",
-    "company": "@github",
-    "blog": "https://github.blog",
-    "location": "San Francisco",
-    "email": null,
-    "hireable": null,
-    "bio": null,
-    "twitter_username": null,
-    "public_repos": 8,
-    "public_gists": 8,
-    "followers": 3200,
-    "following": 9,
-    "created_at": "2011-01-25T18:44:36Z",
-    "updated_at": "2020-08-23T14:28:27Z"
+var response = {}
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+
+
+var formSubmitHandler = function(event){
+    event.preventDefault();
+
+    var username = nameInputEl.value.trim();
+
+    if(username){
+        getUserRepos(username);
+        nameInputEl.value = "";
+    }else{
+        alert("please enter github username");
     }
+    console.log(event);
+}
 
-    var getUserRepos = function(user) {
-        // format the github api url
-        var apiUrl = "https://api.github.com/users/" + user + "/repos";
+var getUserRepos = function(user) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
       
-        // make a request to the url
-        fetch(apiUrl).then(function(response) {
-          response.json().then(function(data) {
-            console.log(data);
-          });
-        });
-      };
+    // make a request to the url
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+        console.log(data);
+      });
+    });
+};
 
-console.log(response.location);
-console.log(response.followers);
+
 getUserRepos("microsoft");
+
+userFormEl.addEventListener("submit", formSubmitHandler);
